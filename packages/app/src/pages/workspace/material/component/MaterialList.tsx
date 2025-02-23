5d89fad91f949fa76ca047305560f046db76285c
tanchaochao <paner1993@163.com>
2025-02-23 21:28:14 +0800

    feat：样式修改

--- packages/app/src/pages/workspace/material/component/MaterialList.tsx
+++ packages/app/src/pages/workspace/material/component/MaterialList.tsx
@@ -1,24 +1,34 @@
-import { Select, DatePicker, Button, Form, Input } from "antd";
+import { Row, Col } from "antd";
 import React from "react";
 import "./index.less";
 
 const Main: React.FC = () => {
   return (
     <div className="material-list">
-      {Array(7).fill(null).map(() => {
-        return (
-          <div className="material-item">
-            <div className="material-item-img mb-4">
-              <img
-                src="https://static.yximgs.com/udata/pkg/admin-center/311250d4d5d3e7a935460a6f55c9faccbb44303b.png"
-                alt="img"
-              />
-            </div>
-            <p className="text-sm mb-2 text-slate-950 truncate">cat catcatcatcatcatcatcatcatcatcatcatcatcatcat</p>
-            <p className="text-xs mb-3 text-gray-400 truncate">今日 20:44</p>
-          </div>
-        );
-      })}
+      <Row>
+        {Array(7)
+          .fill(null)
+          .map(() => {
+            return (
+              <Col xxl={{span:3}} xl={{span:4}}>
+                <div className="material-item">
+                  <div className="material-item-img mb-4">
+                    <img
+                      src="https://static.yximgs.com/udata/pkg/admin-center/311250d4d5d3e7a935460a6f55c9faccbb44303b.png"
+                      alt="img"
+                    />
+                  </div>
+                  <p className="text-sm mb-2 text-slate-950 truncate">
+                    cat catcatcatcatcatcatcatcatcatcatcatcatcatcat
+                  </p>
+                  <p className="text-xs pb-3 text-gray-400 truncate">
+                    今日 20:44
+                  </p>
+                </div>
+              </Col>
+            );
+          })}
+      </Row>
     </div>
   );
 };
