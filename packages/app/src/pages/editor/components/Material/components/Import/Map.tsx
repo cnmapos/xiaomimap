04d2268bde9f008e7ea11c050bc08de41d103c5e
Tanchao <paner1993@163.com>
2025-03-08 22:52:28 +0800

    feat：地图

--- packages/app/src/pages/editor/components/Material/components/Import/Map.tsx
+++ packages/app/src/pages/editor/components/Material/components/Import/Map.tsx
@@ -1,21 +1,35 @@
-import { AppstoreAddOutlined } from "@ant-design/icons";
-import { Dropdown, Space } from "antd";
+import { createViewer, HZViewer } from "@hztx/core";
+import { useEffect, useRef } from "react";
 import classNames from "classnames";
-
 import MapMenuBar from "./MapMenuBar";
 import Search from "./MapSearch";
 import { useState } from "react";
 
 const Map: React.FC = () => {
   const [fullscreen, setFullscreen] = useState(false);
-
+  const container = useRef<HTMLDivElement | null>(null);
+  const context = useRef<{ viewer: HZViewer | null }>({ viewer: null });
+  useEffect(() => {
+    const viewer = createViewer(container.current!, {
+      key: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI5YjliYjIwYi0zMWE0LTQ4MTgtYWU4NC0wNWZmNTFmZjVhYmMiLCJpZCI6MjY1NzYxLCJpYXQiOjE3MzU1NzA3MTl9.BOJDK-WqsLV-QcQhbnAEf-wG1mtkftG1BYV6JIv0VoI",
+    });
+    context.current.viewer = viewer;
+    return () => {
+      viewer?.destroy();
+    };
+  }, []);
   return (
-    <div className={classNames("bg-editor-card text-white absolute w-full h-full", {
-      "fixed top-0 left-0 right-0 bottom-0 z-10": fullscreen,
-    })}>
-      <MapMenuBar ></MapMenuBar>
+    <div
+      className={classNames(
+        "bg-editor-card text-white absolute w-full h-full",
+        {
+          "fixed top-0 left-0 right-0 bottom-0 z-10": fullscreen,
+        }
+      )}
+    >
+      <MapMenuBar></MapMenuBar>
       <Search></Search>
-     <div className="map bg-amber-200 h-full w-full"></div>
+      <div ref={container} className="map h-full w-full"></div>
     </div>
   );
 };
